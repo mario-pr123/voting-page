b1353fe236e11fa837319900bc81982cad7d06a7
Mario Parreño <mario-andree@hotmail.com>
2022-05-03 21:25:29 -0500

    Cambios sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './sponsors.css'
+import sponsor1 from "../../img/sponsor1.png"
 
 export default function Sponsors() {
   return (
     <div className='s'>
       <h1>PATROCINADORES</h1>
       <div className='s-wrapper'>
-          
+          <img src={sponsor1} alt="" />
       </div>
     </div>
   )
