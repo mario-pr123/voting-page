2cf24a5cae362f870f242df845aea3a2f2ae1657
Mario Parreño <mario-andree@hotmail.com>
2022-05-17 21:21:06 -0500

    sponso3

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -2,14 +2,16 @@ import React, { useEffect } from "react";
 import "./sponsors.css";
 import sponsor1 from "../../img/sponsor1.png";
 import sponsor2 from "../../img/sponsor2.png";
+import sponsor3 from "../../img/sponsor3.png";
 import "aos/dist/aos.css";
 import Aos from "aos";
+
 
 export default function Sponsors() {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
-  const sponsors = [{ image: sponsor1 }, { image: sponsor2 }];
+  const sponsors = [{ image: sponsor1 }, { image: sponsor2 },{ image: sponsor3 }];
   return (
     <div className="s">
       <div className="s-wrapper" data-aos="zoom-out">
