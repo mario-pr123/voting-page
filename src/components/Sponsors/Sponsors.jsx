bda80eb00a466deef470b229dfa1360ab5ca117e
Mario Parreño <mario-andree@hotmail.com>
2022-05-27 12:19:32 -0500

    nuevos sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -3,15 +3,22 @@ import "./sponsors.css";
 import sponsor1 from "../../img/sponsor1.png";
 import sponsor2 from "../../img/sponsor2.png";
 import sponsor3 from "../../img/sponsor3.png";
+import utpl from "../../img/utpl.png";
+import fundel from "../../img/fundel.png";
 import "aos/dist/aos.css";
 import Aos from "aos";
-
 
 export default function Sponsors() {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
-  const sponsors = [{ image: sponsor1 }, { image: sponsor2 },{ image: sponsor3 }];
+  const sponsors = [
+    { image: sponsor1 },
+    { image: sponsor2 },
+    { image: sponsor3 },
+    { image: utpl },
+    { image: fundel },
+  ];
   return (
     <div className="s">
       <div className="s-wrapper" data-aos="zoom-out">
