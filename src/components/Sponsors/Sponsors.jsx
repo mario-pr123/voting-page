94396eed8ffdb5429fd3bb3f44c6c864bab129ab
Mario Parreño <mario-andree@hotmail.com>
2022-05-29 22:13:50 -0500

    nuevos sponsors

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -3,6 +3,7 @@ import "./sponsors.css";
 import sponsor1 from "../../img/sponsor1.png";
 import sponsor2 from "../../img/sponsor2.png";
 import sponsor3 from "../../img/sponsor3.png";
+import sponsor4 from "../../img/sponsor4.png";
 import utpl from "../../img/utpl.png";
 import fundel from "../../img/fundel.png";
 import "aos/dist/aos.css";
@@ -19,12 +20,20 @@ export default function Sponsors() {
     { image: utpl },
     { image: fundel },
   ];
+  const sponsors2 = [{ image: sponsor4 }];
   return (
     <div className="s">
       <div className="s-wrapper" data-aos="zoom-out">
         <h1>PATROCINADORES</h1>
         <div className="images">
           {sponsors.map((sp) => {
+            return (
+              <div className="img-wrapper">
+                <img src={sp.image} alt="" />
+              </div>
+            );
+          })}
+          {sponsors2.map((sp) => {
             return (
               <div className="img-wrapper">
                 <img src={sp.image} alt="" />
